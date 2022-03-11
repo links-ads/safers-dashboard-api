2a4c4bd5ca81e7a2f9768f3077f3ecb70d626597
Allyn Treshansky <allyn.treshansky@gmail.com>
2022-03-11 09:40:03 +0000

    email

--- server/config/settings/deployment.py
+++ server/config/settings/deployment.py
@@ -11,20 +11,39 @@ CORS_ORIGIN_ALLOW_ALL = True
 # email #
 #########
 
-EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+# TODO: I ACTUALLY WANT TO USE A "PROPER ESP VIA ANYMAIL
+# TODO: (LIKE SENDMAIL VIA HEROKU), BUT IT TAKES TIME TO
+# TODO: SET UP PROPERLY.  SO FOR NOW I'M USING GMAIL :(
+# EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+# DEFAULT_FROM_EMAIL = "noreply@safers.com"
+# SERVER_EMAIL = "noreply@safers.com"
+# ANYMAIL = {
+#     "MAILGUN_API_KEY":
+#         env("MAILGUN_API_KEY", default=""),
+#     "MAILGUN_DOMAIN":
+#         env("MAILGUN_DOMAIN", default=""),
+#     "MAILGUN_PUBLIC_KEY":
+#         env("MAILGUN_PUBLIC_KEY", default=""),
+#     "MAILGUN_SMTP_LOGIN":
+#         env("MAILGUN_SMTP_LOGIN", default=""),
+#     "MAILGUN_SMTP_PASSWORD":
+#         env("MAILGUN_SMTP_PASSWORD", default=""),
+#     "MAILGUN_SMTP_PORT":
+#         env("MAILGUN_SMTP_PORT", default=""),
+#     "MAILGUN_SMTP_SERVER":
+#         env("MAILGUN_SMTP_SERVER", default=""),
+#     "MAILGUN_WEBHOOK_SIGNING_KEY":
+#         env("MAILGUN_WEBHOOK_SIGNING_KEY", default=""),
+#     "MAILGUN_SENDER_DOMAIN":
+#         env("MAILGUN_SENDER_DOMAIN", default=""),
+# }
 
-DEFAULT_FROM_EMAIL = "noreply@safers.com"
-SERVER_EMAIL = "noreply@safers.com"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ANYMAIL = {
-    "MAILGUN_API_KEY": env("MAILGUN_API_KEY", default=""),
-    # don't need these yet...
-    # "MAILGUN_DOMAIN":
-    #     env("MAILGUN_DOMAIN", default=""),
-    # "MAILGUN_PUBLIC_KEY":
-    #     env("MAILGUN_PUBLIC_KEY", default=""),
-    # "MAILGUN_WEBHOOK_SIGNING_KEY":
-    #     env("MAILGUN_WEBHOOK_SIGNING_KEY", default=""),
-    # "MAILGUN_SENDER_DOMAIN":
-    #     env("MAILGUN_SENDER_DOMAIN", default=""),
-}
+EMAIL_HOST = env("EMAIL_HOST", default="")
+EMAIL_PORT = env("EMAIL_HOST_PORT", default=587)
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
+
+DEFAULT_FROM_EMAIL = env("EMAIL_HOST_USER", default=EMAIL_HOST_USER)
