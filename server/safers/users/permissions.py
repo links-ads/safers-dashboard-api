687b06670d527b4f7073e25ebeb036db229f8a6e
Allyn Treshansky <allyn.treshansky@gmail.com>
2022-04-05 11:03:24 +0100

    updated local/remote permission messages

--- server/safers/users/permissions.py
+++ server/safers/users/permissions.py
@@ -15,12 +15,16 @@ class IsOwnerorAdmin(BasePermission):
 
 
 class IsRemote(BasePermission):
+    message = "Only remote users have permission to perform this action."
+
     def has_permission(self, request, view):
         user = request.user
         return user.is_authenticated and user.is_remote
 
 
 class IsLocal(BasePermission):
+    message = "Only local users have permission to perform this action."
+
     def has_permission(self, request, view):
         user = request.user
         return user.is_authenticated and user.is_local
