bb2ec680963cff3a5b2790639d0b8dd60b57fd78
Allyn Treshansky <allyn.treshansky@gmail.com>
2022-05-06 12:21:35 +0100

    added django-settings env var to rmq server

--- server/safers/rmq/server.py
+++ server/safers/rmq/server.py
@@ -2,17 +2,21 @@
 
 # Script to run the RMQ App as a standalone server
 
+
 def start_rmq():
     from safers.rmq import RMQ
     rmq = RMQ()
     rmq.subscribe()
 
+
 if __name__ == "__main__":
+    import os
     import sys
     import logging
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
     import django
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     django.setup()
 
     start_rmq()
