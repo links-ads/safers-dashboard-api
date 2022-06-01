7b8960198e6032d23c96b7a6fed97802b4bc5a70
Allyn Treshansky <allyn.treshansky@gmail.com>
2022-06-01 16:34:17 +0100

    feat(backend): added UnderspecifiedDateTimeField

--- server/safers/core/fields.py
+++ server/safers/core/fields.py
@@ -12,11 +12,34 @@ class SimplifiedGeometryField(serializers.Field):
         self.geometry_class = kwargs.pop("geometry_class")
         super().__init__(*args, **kwargs)
 
-    def to_representation(self, value):
-        return map(lambda x: round(x, self.precision), value.coords)
-
     def to_internal_value(self, data):
         try:
             return self.geometry_class(data)
         except TypeError as e:
             raise serializers.ValidationError(str(e))
+
+    def to_representation(self, value):
+        return map(lambda x: round(x, self.precision), value.coords)
+
+
+class UnderspecifiedDateTimeField(serializers.DateTimeField):
+    """
+    resets DataTime according to kwargs
+    """
+    def __init__(self, *args, **kwargs):
+
+        self.replacements = {
+            arg: kwargs.pop(arg)
+            for arg in ["year", "month", "day", "hour", "minute", "second", "microsecond"]
+            if arg in kwargs
+        }  # yapf: disable
+
+        super().__init__(format, *args, **kwargs)
+
+    def to_internal_value(self, value):
+        internal_value = super().to_internal_value(value)
+        return internal_value.replace(**self.replacements)
+
+    def to_representation(self, value):
+        representation = super().to_representation(value)
+        return representation.replace(**self.replacements)
