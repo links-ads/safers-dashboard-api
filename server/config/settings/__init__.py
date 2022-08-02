f58869de7de140a223a91634309eaae8d7961da7
Allyn Treshansky <allyn.treshansky@gmail.com>
2022-08-02 15:16:34 +0100

    chore(backend): testing deployment

--- server/config/settings/__init__.py
+++ server/config/settings/__init__.py
@@ -43,6 +43,6 @@ exec(f"from {environment_settings_module} import *") in globals()
 
 import django_on_heroku
 
-# django_on_heroku updates some features I've customised (such as S3 Storage and "postgis" database;
+# django_on_heroku updates some features I've customised (such as S3 Storage and "postgis" database);
 # I can prevent that by adding kwargs as per https://github.com/pkrefta/django-on-heroku#enabling-functionality
 django_on_heroku.settings(locals(), geodjango=True, staticfiles=False)
