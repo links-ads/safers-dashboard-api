f8cc5060cb6430980b7087df4ac7845450114001
Allyn Treshansky <allyn.treshansky@gmail.com>
2022-09-30 14:59:26 +0100

    feat(backend): better error-handling for custom enums

--- server/safers/core/utils/utils_enums.py
+++ server/safers/core/utils/utils_enums.py
@@ -4,34 +4,40 @@ from django.db import models
 class CaseInsensitiveTextChoices(models.TextChoices):
     @classmethod
     def find_enum(cls, string, case_insensitive=True):
-        if string is not None:
+        try:
             if case_insensitive:
                 string = string.upper()
+        except AttributeError as e:
+            # ignore cases where I don't pass an actual string
+            pass
 
-            for name, member in cls.__members__.items():
-                value = member.value
-                if case_insensitive:
-                    value = value.upper()
+        for name, member in cls.__members__.items():
+            value = member.value
+            if case_insensitive:
+                value = value.upper()
 
-                if string == value:
-                    return member
+            if string == value:
+                return member
 
 
 class SpaceInsensitiveTextChoices(models.TextChoices):
     @classmethod
     def find_enum(cls, string, case_insensitive=True, space_insensitive=True):
-        if string is not None:
+        try:
             if case_insensitive:
                 string = string.upper()
             if space_insensitive:
                 string = string.replace(" ", "")
+        except AttributeError as e:
+            # ignore cases where I don't pass an actual string
+            pass
 
-            for name, member in cls.__members__.items():
-                value = member.value
-                if case_insensitive:
-                    value = value.upper()
-                if space_insensitive:
-                    value = value.replace(" ", "")
+        for name, member in cls.__members__.items():
+            value = member.value
+            if case_insensitive:
+                value = value.upper()
+            if space_insensitive:
+                value = value.replace(" ", "")
 
-                if string == value:
-                    return member
+            if string == value:
+                return member
