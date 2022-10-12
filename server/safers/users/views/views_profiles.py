38ee9f2d64cad969322cf12e7dcf827a1382bf13
Allyn Treshansky <allyn.treshansky@gmail.com>
2022-10-12 16:25:04 +0100

    feat(backend): merged remote and local UserProfile data
    Merged local UserProfile data w/ remote UserProfile data and
    synchronised it on the remote authentication server.  This ensures that
    UserProfile data that is not stored locally is not accidentally reset
    remotely.

--- server/safers/users/views/views_profiles.py
+++ server/safers/users/views/views_profiles.py
@@ -9,15 +9,34 @@ from safers.users.authentication import ProxyAuthentication
 def synchronize_profile(user_profile, user_profile_data):
     # TODO: Refactor UserProfile to match FusionAuth
     # TODO: And make this a proper DRF view
+
     user = user_profile.user
-    PROFILE_URL_PATH = "/api/services/app/Profile/UpdateProfile"
+
+    GET_PROFILE_URL_PATH = "/api/services/app/Profile/GetProfile"
+    SET_PROFILE_URL_PATH = "/api/services/app/Profile/UpdateProfile"
+
+    response = requests.get(
+        urljoin(
+            settings.SAFERS_GATEWAY_API_URL,
+            GET_PROFILE_URL_PATH,
+        ),
+        auth=ProxyAuthentication(user)
+    )
+    response.raise_for_status()
+
+    # merge remote profile data w/ local profile data...
+    proxy_data = response.json()["profile"]
+    proxy_data["user"].update(user_profile_data["user"])
+    proxy_data["organizationId"] = user_profile_data["organizationId"]
+
     response = requests.put(
         urljoin(
             settings.SAFERS_GATEWAY_API_URL,
-            PROFILE_URL_PATH,
+            SET_PROFILE_URL_PATH,
         ),
         auth=ProxyAuthentication(user),
-        json=user_profile_data
+        json=proxy_data
     )
     response.raise_for_status()
+
     return response.json()
