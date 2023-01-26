3f91bb532e4a128b57e79943ee5cccb22d9499fa
Allyn Treshansky <allyn.treshansky@gmail.com>
2023-01-26 20:40:59 +0000

    testing db-backup config

--- server/config/settings/deployment.py
+++ server/config/settings/deployment.py
@@ -110,11 +110,11 @@ if SENTRY_DSN:
 # backups #
 ###########
 
-DBBACKUP_STORAGE = "safers.core.storage.PrivateMediaS3Storage"
+DBBACKUP_STORAGE = "safers.core.storage.PublicMediaS3Storage"
 DBBACKUP_STORAGE_OPTIONS = {
     "access_key": AWS_ACCESS_KEY_ID,
     "secret_key": AWS_SECRET_ACCESS_KEY,
     "bucket_name": AWS_STORAGE_BUCKET_NAME,
-    "default_acl": PRIVATE_MEDIA_DEFAULT_ACL,
+    "default_acl": "private",
     "location": "backups/",
 }
