745b677fe6925cac1fd21e93284bc9ff4aa5859a
Allyn Treshansky <allyn.treshansky@gmail.com>
2023-05-24 12:51:27 +0100

    feat(backend): renamed config view to settings view
    Renamed config view to settings view.  Because that's what it is - a
    view of the SafersSettings instance.

--- server/safers/core/views/views_settings.py
+++ server/safers/core/views/views_settings.py
@@ -3,21 +3,18 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from drf_yasg.utils import swagger_auto_schema
-
-from silk.profiling.profiler import silk_profile 
+from drf_spectacular.utils import extend_schema
 
 from safers.core.models import SafersSettings
 from safers.core.serializers import SafersSettingsSerializer
 
 
-@silk_profile()
-@swagger_auto_schema(
-    methods=["GET"], responses={status.HTTP_200_OK: SafersSettingsSerializer}
-)
+@extend_schema(responses={
+    status.HTTP_200_OK: SafersSettingsSerializer,
+})
 @permission_classes([AllowAny])
 @api_view(["GET"])
-def config_view(request):
+def settings_view(request):
     """
     Returns some information required by the client for initial configuration
     """
