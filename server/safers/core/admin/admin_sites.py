e705eb360c402d20de7ad6380c64acc8b8dc9e8a
Allyn Treshansky <allyn.treshansky@gmail.com>
2023-05-26 14:18:57 +0100

    feat(backend): added colors to SiteProfile
    Added colors to SiteProfile to allow me to easily distinguish different
    sites.

--- server/safers/core/admin/admin_sites.py
+++ server/safers/core/admin/admin_sites.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from django.contrib.sites.models import Site
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 
 from django.urls import reverse
 from django.utils.html import format_html
 
-from safers.core.admin.admin_utils import get_clickable_fk_list_display
 from safers.core.models import SiteProfile
 
 
@@ -15,13 +13,15 @@ class SiteProfileAdminForm(ModelForm):
         model = SiteProfile
         fields = (
             "site",
-            "code",
             "description",
+            "code",
+            "show_admin_notice",
+            "admin_notice_color",
         )
 
     def clean_code(self):
         from safers.data.models.models_maprequests import REQUEST_ID_SEPARATOR
-        code = self.cleaned_data["code"]
+        code = self.cleaned_data.get("code")
         if code and REQUEST_ID_SEPARATOR in code:
             raise ValidationError(
                 f"code cannot include the character: '{REQUEST_ID_SEPARATOR}'."
@@ -32,10 +32,21 @@ class SiteProfileAdminForm(ModelForm):
 @admin.register(SiteProfile)
 class SiteProfileAdmin(admin.ModelAdmin):
     form = SiteProfileAdminForm
-    list_display = ("code", "get_site_for_list_display")
+    list_display = (
+        "get_name_for_list_display",
+        "code",
+        "get_site_for_list_display",
+    )
+
+    @admin.display(description="PROFILE")
+    def get_name_for_list_display(self, obj):
+        return str(obj.site.name)
 
     @admin.display(description="SITE")
     def get_site_for_list_display(self, obj):
+        # not using "get_clickable_fk_for_list_display"
+        # b/c the Site Model uses a non-standard admin change URL
+        site = obj.site
         admin_change_url_name = "admin:sites_site_change"
-        list_display = f"<a href='{reverse(admin_change_url_name, args=[obj.site.pk])}'>{str(obj.site)}</a>"
+        list_display = f"<a href='{reverse(admin_change_url_name, args=[site.pk])}'>{str(site)}</a>"
         return format_html(list_display)
--- server/safers/core/models/models_sites.py
+++ server/safers/core/models/models_sites.py
@@ -1,5 +1,18 @@
 from django.contrib.sites.models import Site
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from colorfield.fields import ColorField
+
+
+class AdminNoticeColorSamples(models.TextChoices):
+    """
+    Suggested colors for the site-specific Admin Notice.
+    """
+    DEVELOPMENT = "#CCCCCC", _("Grey")
+    TESTING = "#009933", _("Green")
+    STAGING = "#FFAA00", _("Yellow")
+    PRODUCTION = "#FF0000", _("Red")
 
 
 class SiteProfile(models.Model):
@@ -8,11 +21,44 @@ class SiteProfile(models.Model):
         verbose_name_plural = "Site Profiles"
 
     site = models.OneToOneField(
-        Site, on_delete=models.CASCADE, related_name="profile"
+        Site,
+        on_delete=models.CASCADE,
+        related_name="profile",
+        help_text=_("Associated site."),
     )
 
-    code = models.CharField(max_length=8, unique=True, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(
+        blank=True,
+        null=True,
+        help_text=_("Description of site."),
+    )
 
-    def __str__(self):
+    code = models.CharField(
+        max_length=8,
+        unique=True,
+        blank=True,
+        null=True,
+        help_text=_("Unique code to identify this site (to RMQ)."),
+    )
+
+    show_admin_notice = models.BooleanField(
+        default=False,
+        help_text=_(
+            "Whether or not to show a site-specific notice in the Admin."
+        ),
+    )
+
+    admin_notice_color = ColorField(
+        default=AdminNoticeColorSamples.DEVELOPMENT,
+        samples=AdminNoticeColorSamples.choices,
+        help_text=_("Color of site-specific notice in the Admin."),
+    )
+
+    def __str__(self) -> str:
         return str(self.site)
+
+    def save(self, *args, **kwargs):
+        # if a SiteProfile has changed, make sure the next time current_site
+        # is accessed it is taken from the db rather than from the cache.
+        Site.objects.clear_cache()
+        return super().save(*args, **kwargs)
