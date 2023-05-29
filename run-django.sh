bcc7a209503ace56878df7723586d79d03d742f7
Mark Small <mark.small@astrosat.net>
2023-05-29 10:22:35 +0100

    fix(backend): Ensure cache tables created
    When starting from scratch, the backend was trying to access tables
    before they were created e.g. during the migration phase, but at
    that point, the cache tables didn't yet exist. I switched the cache
    tables creation to be the first thing in the script and it all now
    seems to work as expected.

--- run-django.sh
+++ run-django.sh
@@ -5,8 +5,8 @@ until echo > /dev/tcp/safers-db/5432; do sleep 1; done
 
 cd $APP_HOME/server
 
+setuser app pdm run ./manage.py createcachetable
 setuser app pdm run ./manage.py migrate
 setuser app pdm run ./manage.py collectstatic --no-input --link
-setuser app pdm run ./manage.py createcachetable
 
 exec /sbin/setuser app pdm run ./manage.py runserver 0.0.0.0:8000
