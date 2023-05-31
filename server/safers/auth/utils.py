4293e2922576eb65a1e4faeda2019be237949e4e
Allyn Treshansky <allyn.treshansky@gmail.com>
2023-05-31 11:31:58 +0100

    feat(backend): enable deleting user
    Enable deleting user locally and from FusionAuth.
    
    Also improved the reshape_auth_errors function so theat missing
    "non_form_field" errors are not included.

--- server/safers/auth/utils.py
+++ server/safers/auth/utils.py
@@ -7,15 +7,17 @@ def reshape_auth_errors(auth_error_response):
     DRF
     """
     drf_error_response = {}
+
     field_errors = auth_error_response.get("fieldErrors", [])
-    non_field_errors = auth_error_response.get("error_description")
-    if field_errors or non_field_errors:
+    if field_errors:
         drf_error_response.update({
             field: [error.get("message") for error in errors]
             for field,
             errors in field_errors.items()
         })
-        drf_error_response[drf_settings.NON_FIELD_ERRORS_KEY] = non_field_errors
-        return drf_error_response
 
-    return auth_error_response
+    non_field_errors = auth_error_response.get("error_description")
+    if non_field_errors:
+        drf_error_response[drf_settings.NON_FIELD_ERRORS_KEY] = non_field_errors
+
+    return drf_error_response or auth_error_response
