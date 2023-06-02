e4597ec6ce24dcca5ffa4fa789e5f3cfbb1714b8
Allyn Treshansky <allyn.treshansky@gmail.com>
2023-06-02 12:18:30 +0100

    feat(backend): added FusionAuth settings to the config endpoint
    Added FusionAuth settings (like the time_to_live of the access_token and
    refresh_token) to the output of the "/api/config" endpoint.
    
    Also wrapped the existing AUTH_CLIENT in a class so I can add extra
    methods as needed.

--- server/safers/auth/clients.py
+++ server/safers/auth/clients.py
@@ -1,7 +1,40 @@
 from django.conf import settings
+from django.core.exceptions import BadRequest
+from django.utils.functional import cached_property
 
 from fusionauth.fusionauth_client import FusionAuthClient
 
-AUTH_CLIENT = FusionAuthClient(
+from safers.auth.utils import reshape_auth_errors
+
+
+class SafersFusionAuthClient(FusionAuthClient):
+    """
+    A wrapper around the existing FusionAuthClient to add some custom
+    safers-specific functionality
+    """
+    @cached_property
+    def settings(self):
+        """
+        Returns information about how FusionAuth is configured
+        """
+        auth_response = self.retrieve_application(
+            settings.FUSIONAUTH_APPLICATION_ID
+        )
+        if not auth_response.was_successful():
+            errors = reshape_auth_errors(auth_response.error_response)
+            raise BadRequest(errors)
+
+        auth_application = auth_response.success_response.get("application")
+        settings_data = auth_application.get("jwtConfiguration", {})
+
+        return {
+            "refresh_token_expiration_policy": settings_data["refreshTokenExpirationPolicy"],
+            "refresh_token_usage_policy": settings_data["refreshTokenUsagePolicy"],
+            "refresh_token_time_to_live": settings_data["refreshTokenTimeToLiveInMinutes"] * 60,
+            "access_token_time_to_live": settings_data["timeToLiveInSeconds"],
+        }  # yapf: disable
+
+
+AUTH_CLIENT = SafersFusionAuthClient(
     settings.FUSIONAUTH_API_KEY, settings.FUSIONAUTH_INTERNAL_URL
 )
--- server/safers/core/serializers/serializers_settings.py
+++ server/safers/core/serializers/serializers_settings.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 
+from drf_spectacular.utils import extend_schema_field
+
+from safers.auth.clients import AUTH_CLIENT
+
 from safers.core.models import SafersSettings
 
 
@@ -16,4 +20,19 @@ class SafersSettingsSerializer(serializers.ModelSerializer):
             "request_timeout",
             "max_favorite_alerts",
             "max_favorite_events",
+            "auth_settings",
         )
+
+    auth_settings = serializers.SerializerMethodField()
+
+    @extend_schema_field({
+        "type": "object",
+        "example": {
+            "refresh_token_expiration_policy": "Fixed",
+            "refresh_token_usage_policy": "Reusable",
+            "refresh_token_time_to_live": 43200,
+            "access_token_time_to_live": 3600,
+        }
+    })
+    def get_auth_settings(self, obj):
+        return AUTH_CLIENT.settings
