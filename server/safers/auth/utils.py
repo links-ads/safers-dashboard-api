cbde803fcfa688a928ea713593a564a6ae63744b
Allyn Treshansky <allyn.treshansky@gmail.com>
2023-06-12 11:32:05 +0100

    feat(backend): better error handling for auth errors

--- server/safers/auth/utils.py
+++ server/safers/auth/utils.py
@@ -8,16 +8,20 @@ def reshape_auth_errors(auth_error_response):
     """
     drf_error_response = {}
 
-    field_errors = auth_error_response.get("fieldErrors", [])
-    if field_errors:
-        drf_error_response.update({
-            field: [error.get("message") for error in errors]
-            for field,
-            errors in field_errors.items()
-        })
+    try:
+        field_errors = auth_error_response.get("fieldErrors", [])
+        if field_errors:
+            drf_error_response.update({
+                field: [error.get("message") for error in errors]
+                for field,
+                errors in field_errors.items()
+            })
 
-    non_field_errors = auth_error_response.get("error_description")
-    if non_field_errors:
-        drf_error_response[drf_settings.NON_FIELD_ERRORS_KEY] = non_field_errors
+        non_field_errors = auth_error_response.get("error_description")
+        if non_field_errors:
+            drf_error_response[drf_settings.NON_FIELD_ERRORS_KEY
+                              ] = non_field_errors
+    except Exception:
+        pass
 
     return drf_error_response or auth_error_response
